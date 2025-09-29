d7612d6cae12441a3e9a53a9c39fcb080a57e3a6
Masterlu22 <lucasrochanilo@gmail.com>
2025-09-29 11:13:37 -0300

    Adicionado comnetários para javaDoc

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,12 +1,20 @@
 public class Calculadora{
+    /**
+     * Esta função tem como papel retornar verdadeiro quando um número é par e positivo, e falso quando o contrário
+     * @param numero - valores inteiros
+     * @return true - boolean
+     * @return false - boolean
+     * @author Lucas Da Silva Rocha
+     * @version 3.0.0
+     */
     public static boolean verificaValor(double numero){
+        //verifica se o número é positivo e se o mesmo é par
         if(numero >= 0 && numero % 2 == 0){
             return true;
         }
         else{
             return false;
         }
-
     }
 
 }
